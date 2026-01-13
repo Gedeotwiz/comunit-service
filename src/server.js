d82652a64f079dde911b0ea192c48bcfbe45f343
Twizerimana <gedeontwizerimana6@gmail.com>
2026-01-13 17:18:31 +0300

    community service project

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+
+dotenv.config()
+
+
+const app = express()
+
+const port = process.env.PORT
+const db = process.env.DATABASE
+
+app.listen(port,()=>{
+    console.log(`Server running on ${port}`)
+})
+
+mongoose.connect(db).then(()=>{console.log("Database connected successfuly!!")})
+.catch((error)=>{console.log(`Error is ${error}`)})
