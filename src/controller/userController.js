bf4539b3e902d18c488307f034241b11dcfa2301
Twizerimana <gedeontwizerimana6@gmail.com>
2026-01-19 17:13:18 +0300

    work on verify access

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,10 +1,15 @@
 import User from "../model/userModel.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+
 class Controller{
 
  static signup=async(req,res)=> {
       const {names,email,password,role} = req.body
+       const hashPassword = bcrypt.hashSync(req.body.password,10)
       try {
-          const user = await User.create({names,email,password,role})
+          const user = await User.create({names,email,password:hashPassword,role})
       if(!user){
          return res.status(404).json({message:"user not found"})
       }else{
@@ -17,7 +22,59 @@ class Controller{
      
 }
 
-   
+static login = async(req,res)=>{
+   const {email,password} = req.body
+   const user = await User.findOne({email})
+   if(!user){
+      return res.status(404).json({message:"Invalid email"})
+   }else{
+      const comparePassword = bcrypt.compareSync(password,user.password)
+      if(!comparePassword){
+         return res.status(404).json({message:"Invald password"})
+      }else{
+         const token = jwt.sign({user:user},process.env.SCRET_KEY,{expiresIn:"1d"})
+         return res.status(201).json({message:"Login successfuly",token})
+      }
+   }
+}
+
+static getAllUser = async(req,res)=>{
+   const users = await User.find()
+   if(!users){
+      return res.status(404).json({message:'User not found'})
+   }else{
+      return res.status(200).json({message:"user successfuly retrived",users})
+   }
+}
+
+static deletAllUser = async(req,res)=>{
+   const users = await User.deleteMany()
+   if(!users){
+      return res.status(404).json({message:'User not found'})
+   }else{
+      return res.status(200).json({message:"user successfuly deleted",users})
+   }
+}
+
+static getOneUser = async(req,res)=>{
+   const id = req.params.id
+   const user = await User.findById(id)
+   if(!user){
+       return res.status(404).json({message:'User not found'})
+   }else{
+      return res.status(200).json({message:"user successfuly retrived",user})
+   }
+}
+
+static updateUser = async(req,res)=>{
+   const id = req.params.id
+   const user = await User.findByIdAndUpdate(id,req.body,{new:true})
+   if(!user){
+       return res.status(404).json({message:'User not found'})
+   }else{
+      return res.status(201).json({message:"user successfuly updated",user})
+   }
+}
 
 }
 export default Controller
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,14 @@
 import express from "express"
 import Controller from "../controller/userController.js"
+import { EmailExist } from "../midleware/validation.js"
+import { VerifyAccess } from "../midleware/virifyAccess.js"
 
 
 const router = express.Router()
-router.post("/user",Controller.signup)
+router.post("/user",EmailExist,Controller.signup)
+router.post("/user/login",Controller.login)
+router.get("/users",VerifyAccess("admin"),Controller.getAllUser)
+router.delete("/users",Controller.deletAllUser)
+router.get("/user/:id",Controller.getOneUser)
+router.patch("/user/:id",Controller.updateUser)
 export default router
